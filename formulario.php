f3a8c3868df4bbb521707faa2ee3427f7ec708d7
JoelMendozaa <yowismr@gmail.com>
2024-10-10 08:31:01 +0100

    Añadiendo formulario.php con contenido html

---
+++ formulario.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="formulario.php" method="post" value="">
+        Nombre: <input type="text" name="name">
+        Email:  <input type="email" name="email">
+        Edad:   <input type="number" name="edad">
+        Pais:   <input type="text" name="pais">
+        <input type="submmit">
+    </form>  
+</body>
+</html>
