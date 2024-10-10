56fcc74e59c0f219b85a0fc26e0e988ed8ad7ffd
JoelMendozaa <yowismr@gmail.com>
2024-10-10 09:03:14 +0100

    Se ha corregido errores, comienzo de validacion php

--- formulario.php
+++ formulario.php
@@ -1,8 +1,5 @@
-<?php
 
 
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,11 +10,45 @@
 </head>
 <body>
     <form action="formulario.php" method="post" value="">
-        Nombre: <input type="text" name="name">
+        Nombre: <input type="text" name="nombre">
         Email:  <input type="email" name="email">
         Edad:   <input type="number" name="edad">
         Pais:   <input type="text" name="pais">
-        <input type="submmit">
+        <button type="submmit">Enviar</button>
     </form>  
+
+
+    <?php
+
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $edad = $_POST["edad"];
+    $pais = $_POST["pais"];
+
+    if (preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/", $email)) {
+       echo("El email es valido");
+    } else {
+        echo("Error de email");
+    }
+
+    if (preg_match("/^[a-zA-Z0-9._-]$/", $nombre)) {
+        echo("El nombre es valido");
+     } else {
+        echo "Error de nombre";
+     }
+
+
+     if (preg_match("/^[0-9]$/", $edad)) {
+        echo("La edad es valida");
+     } else {
+        echo ("Error de edad");
+     }
+ 
+     if (preg_match("/^[a-zA-Z]$/", $pais)) {
+         echo("El pais es valido");
+      } else {
+        echo ("Error de pais");
+      }
+?>
 </body>
 </html>
